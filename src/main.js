e3e6aa8042a8dee5423e72289d3d6ed25413c6fd
beardemo11 <a22031227@gmail.com>
2021-07-08 12:11:40 +0800

    更新MAIN.js

--- src/main.js
+++ src/main.js
@@ -1,5 +1,23 @@
 import { createApp } from 'vue'
+import { Field, Form, ErrorMessage, defineRule, configure } from 'vee-validate'
+import { required, email, min } from '@vee-validate/rules'
+import { localize, setLocale } from '@vee-validate/i18n'
+import zhTw from '@vee-validate/i18n/dist/locale/zh_TW.json'
 import App from './App.vue'
 import router from './router'
 
-createApp(App).use(router).mount('#app')
+defineRule('required', required)
+defineRule('email', email)
+defineRule('min', min)
+configure({
+  generateMessage: localize({ zh_TW: zhTw }),
+  validateOnInput: true
+})
+setLocale('zh_TW')
+
+const app = createApp(App).use(router)
+app.component('Form', Form)
+app.component('Field', Field)
+app.component('ErrorMessage', ErrorMessage)
+
+app.mount('#app')
