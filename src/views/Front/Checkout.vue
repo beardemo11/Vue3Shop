c05fc9d127a5a76510770fbd6a37bb5222aa54b7
beardemo11 <a22031227@gmail.com>
2021-09-02 18:59:39 +0800

    訂單資訊樣式、內容做修正

--- src/views/Front/Checkout.vue
+++ src/views/Front/Checkout.vue
@@ -1,19 +1,19 @@
 <template>
   <div class="container-fluid bg-light py-5">
     <div class="col-md-6 m-auto text-center">
-      <h1 class="h1 fw-bold">購物明細</h1>
-      <p>測試</p>
+      <h1 class="h1 fw-bold">填寫訂單資料</h1>
+      <p class="fw-bold">填寫購買人資料</p>
     </div>
   </div>
   <div class="container">
-    <div class="pb-5">
+    <div class="py-4 py-md-6">
       <Form v-slot="{ errors }" @submit="createOrder">
         <div class="row gx-lg-5">
           <div class="col-lg-6 order-2 order-lg-1">
-            <h3 class="h5 mb-md-4 py-3 fw-bold">購買者資訊</h3>
+            <h3 class="fw-bold font-md border-bottom pb-3 mb-3">購買者資訊</h3>
 
             <div class="mb-3 mb-lg-4">
-              <label for="name" class="form-label">姓名</label
+              <label for="name" class="form-label fw-bold">姓名</label
               ><Field
                 id="name"
                 name="姓名"
@@ -24,10 +24,13 @@
                 rules="required"
                 v-model="form.user.name"
               ></Field>
-              <ErrorMessage name="姓名" class="invalid-feedback"></ErrorMessage>
+              <ErrorMessage
+                name="姓名"
+                class="invalid-feedback fw-bold"
+              ></ErrorMessage>
             </div>
             <div class="mb-3 mb-lg-4">
-              <label for="email" class="form-label">Email</label>
+              <label for="email" class="form-label fw-bold">Email</label>
               <Field
                 id="email"
                 name="email"
@@ -40,11 +43,11 @@
               ></Field>
               <ErrorMessage
                 name="email"
-                class="invalid-feedback"
+                class="invalid-feedback fw-bold"
               ></ErrorMessage>
             </div>
             <div class="mb-3 mb-lg-4">
-              <label for="tel" class="form-label">收件人電話</label>
+              <label for="tel" class="form-label fw-bold">收件人電話</label>
               <Field
                 id="tel"
                 name="電話"
@@ -55,10 +58,13 @@
                 rules="required"
                 v-model="form.user.tel"
               ></Field>
-              <ErrorMessage name="電話" class="invalid-feedback"></ErrorMessage>
+              <ErrorMessage
+                name="電話"
+                class="invalid-feedback fw-bold"
+              ></ErrorMessage>
             </div>
             <div class="mb-3 mb-lg-4">
-              <label for="address" class="form-label">收件人地址</label>
+              <label for="address" class="form-label fw-bold">收件人地址</label>
               <Field
                 id="address"
                 name="地址"
@@ -69,10 +75,13 @@
                 rules="required"
                 v-model="form.user.address"
               ></Field>
-              <ErrorMessage name="地址" class="invalid-feedback"></ErrorMessage>
+              <ErrorMessage
+                name="地址"
+                class="invalid-feedback fw-bold"
+              ></ErrorMessage>
             </div>
             <div class="mb-3 mb-lg-4">
-              <label for="message" class="form-label">留言</label>
+              <label for="message" class="form-label fw-bold">留言</label>
               <textarea
                 name=""
                 id="message"
@@ -84,56 +93,82 @@
             </div>
 
             <div class="text-end d-lg-none">
-              <button type="submit" class="btn btn-primary px-4 text-white">
-                購買確認</button
-              ><!---->
+              <button type="submit" class="btn btn-success px-4 text-white">
+                購買確認
+              </button>
             </div>
           </div>
-          <div class="col-lg-6 order-1 order-lg-2">
-            <h3 class="h5 mb-md-4 py-3 fw-bold">購買項目</h3>
-            <ul
-              class="
-                bg-light
-                list-group list-group-flush
-                p-2 p-lg-5
-                mb-3 mb-lg-4
-              "
-            >
-              <li class="list-group-item d-flex justify-content-md-between">
-                <p class="fw-bold">名稱 <span class="d-md-none">/</span></p>
-                <p class="fw-bold">金額</p>
-              </li>
-              <li
-                class="list-group-item"
+          <div class="col-md-5 order-1 order-md-2 mb-4 mb-md-0">
+            <div class="border p-2 py-4 p-lg-4">
+              <h3 class="fw-bold font-md border-bottom pb-3 mb-3">購買項目</h3>
+              <div
+                class="d-flex align-items-center justify-content-between mb-2"
                 v-for="cart in cart.carts"
                 :key="cart.id"
               >
-                <div class="d-md-flex justify-content-between mb-md-2">
-                  <p>{{ cart.product.title }} x {{ cart.qty }}</p>
-                  <p>{{ $filters.currency(cart.product.price) }}</p>
+                <div class="d-flex align-items-center">
+                  <img
+                    class="order-img"
+                    :src="cart.product.imageUrl"
+                    :alt="cart.product.title"
+                  />
+                  <div class="p-2">
+                    <h4 class="font-md-normal fw-bold">
+                      {{ cart.product.title }} x {{ cart.qty }}
+                    </h4>
+                    <!-- <h5 class="font-md-normal d-flex align-items-center">
+                      US 8.5 <span class="ms-1"> </span>
+                    </h5>
+                    <h5 class="font-md-normal">顏色：白色</h5> -->
+                  </div>
                 </div>
-              </li>
-
-              <li class="list-group-item d-flex justify-content-between">
-                <p class="fw-bold">小計</p>
-                <p class="fw-bold">NT$ {{ $filters.currency(cart.total) }}</p>
-              </li>
-              <li class="list-group-item d-flex justify-content-between">
-                <p class="fw-bold">折扣</p>
-                <p class="fw-bold">
-                  NT$ {{ $filters.currency(cart.total - cart.final_total) }}
-                </p>
-              </li>
-              <li class="list-group-item d-flex justify-content-between">
-                <p class="fw-bold">總計</p>
-                <p class="fw-bold">
+                <p>NT$ {{ $filters.currency(cart.product.price) }}</p>
+              </div>
+              <p
+                class="
+                  text-end
+                  mb-2
+                  d-flex
+                  justify-content-between
+                  align-items-center
+                  mb-2
+                  fw-bold
+                "
+              >
+                小計： <span>NT$ {{ $filters.currency(cart.total) }} </span>
+              </p>
+              <p
+                class="
+                  text-end
+                  mb-2
+                  d-flex
+                  justify-content-between
+                  align-items-center
+                  fw-bold
+                "
+              >
+                折扣：
+                <span
+                  >NT$ {{ $filters.currency(cart.total - cart.final_total) }}
+                </span>
+              </p>
+              <div
+                class="
+                  d-flex
+                  align-items-center
+                  justify-content-between
+                  border-top
+                  pt-3
+                "
+              >
+                <span class="font-md-md fw-bold">總金額：</span
+                ><span class="font-md-md fw-bold">
                   NT$ {{ $filters.currency(cart.final_total) }}
-                </p>
-              </li>
-            </ul>
-            <div class="text-end d-none d-lg-block">
-              <button type="submit" class="btn btn-success">購買確認</button
-              ><!---->
+                </span>
+              </div>
+            </div>
+            <div class="py-4 text-end d-none d-lg-block">
+              <button type="submit" class="btn btn-success">購買確認</button>
             </div>
           </div>
         </div>
@@ -202,3 +237,10 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.order-img {
+  width: 100px;
+  height: 100px;
+}
+</style>
--- src/views/Front/OrderForm.vue
+++ src/views/Front/OrderForm.vue
@@ -5,94 +5,110 @@
         <i class="bi bi-check-circle-fill text-success" style="top: -10px"></i>
       </p>
       <h1 class="h1 fw-bold">感謝您！您的訂單已建立完成</h1>
-      <p>訂單編號: {{ orderId }}</p>
+      <p class="fw-bold">訂單編號: {{ orderId }}</p>
     </div>
   </div>
-  <div class="container py-lg-4 mb-8 mb-lg-0">
-    <div class="d-lg-flex justify-content-center align-items-center mb-5">
-      <!-- <div>
-        <h5 class="mb-1">感謝您！您的訂單已建立完成</h5>
-        <p class="text-info">訂單編號: {{ orderId }}</p>
-      </div> -->
-    </div>
-    <div class="pb-md-5"><p class="border-bottom-custom"></p></div>
-    <div class="row">
-      <div class="col-lg-6">
-        <div class="mb-6">
-          <h3 class="h6 mb-2 mb-md-3 fw-bold">訂單資訊</h3>
+  <div class="container">
+    <div class="py-4 py-md-6">
+      <div class="row gx-lg-5">
+        <div class="col-lg-6 order-2 order-lg-1">
+          <h3 class="fw-bold font-md border-bottom pb-3 mb-3">訂單資訊</h3>
           <ul class="list-group-flush">
             <li class="d-flex mb-2">
-              <p class="w-25 me-1 me-lg-0 text-info">訂購時間</p>
-              <p class="w-75">
+              <p class="w-25 me-1 me-lg-0 text-success fw-bold">訂購時間</p>
+              <p class="w-75 fw-bold">
                 {{ $filters.date(order.create_at) }}
                 {{ $filters.time(order.create_at) }}
               </p>
             </li>
             <li class="d-flex">
-              <p class="w-25 me-1 me-lg-0 text-info">處理狀態</p>
-              <p class="w-75">處理中</p>
+              <p class="w-25 me-1 me-lg-0 text-success fw-bold">處理狀態</p>
+              <p class="w-75 fw-bold">處理中</p>
             </li>
           </ul>
-        </div>
-        <div class="mb-6">
-          <h3 class="h6 mb-2 mb-md-3 fw-bold">聯絡資訊</h3>
+          <h3 class="fw-bold font-md border-bottom pb-3 mb-3">購買者資訊</h3>
           <ul class="list-group-flush">
             <li class="d-lg-flex mb-1 mb-md-2">
-              <p class="w-25 me-3 me-lg-0 text-info">姓名</p>
-              <p class="w-75">{{ order.user.name }}</p>
+              <p class="w-25 me-3 me-lg-0 text-success fw-bold">姓名</p>
+              <p class="w-75 fw-bold">{{ order.user.name }}</p>
             </li>
             <li class="d-lg-flex mb-2">
-              <p class="w-25 me-3 me-lg-0 text-info">運送地址</p>
-              <p class="w-75">{{ order.user.address }}</p>
+              <p class="w-25 me-3 me-lg-0 text-success fw-bold">運送地址</p>
+              <p class="w-75 fw-bold">{{ order.user.address }}</p>
             </li>
             <li class="d-lg-flex mb-2">
-              <p class="w-25 me-3 me-lg-0 text-info">聯絡電話</p>
-              <p class="w-75">{{ order.user.tel }}</p>
+              <p class="w-25 me-3 me-lg-0 text-success fw-bold">聯絡電話</p>
+              <p class="w-75 fw-bold">{{ order.user.tel }}</p>
             </li>
             <li class="d-lg-flex">
-              <p class="w-25 me-3 me-lg-0 text-info">聯絡信箱</p>
-              <p class="w-75">{{ order.user.email }}</p>
+              <p class="w-25 me-3 me-lg-0 text-success fw-bold">聯絡信箱</p>
+              <p class="w-75 fw-bold">{{ order.user.email }}</p>
             </li>
           </ul>
         </div>
-      </div>
-      <div class="col-lg-6">
-        <div class="mb-6">
-          <h3 class="h6 mb-2 mb-md-3 fw-bold">購買項目</h3>
-          <ul class="list-group list-group-flush">
-            <li v-for="item in order.products" :key="item.id">
-              <div class="d-md-flex justify-content-between mb-md-2">
-                <p>{{ item.product.title }} x {{ item.qty }}</p>
-                <p>NT$ {{ $filters.currency(item.product.price) }}</p>
+        <div class="col-md-5 order-1 order-md-2 mb-4 mb-md-0">
+          <div class="border p-2 py-4 p-lg-4">
+            <h3 class="fw-bold font-md border-bottom pb-3 mb-3">購買項目</h3>
+            <div
+              class="d-flex align-items-center justify-content-between mb-2"
+              v-for="item in order.products"
+              :key="item.id"
+            >
+              <div class="d-flex align-items-center">
+                <img
+                  class="order-img"
+                  :src="item.product.imageUrl"
+                  :alt="item.product.title"
+                />
+                <div class="p-2">
+                  <h4 class="font-md-normal fw-bold">
+                    {{ item.product.title }} x {{ item.qty }}
+                  </h4>
+                </div>
               </div>
-            </li>
-
-            <li class="d-flex justify-content-between">
-              <p>總計</p>
-              <p class="fw-bold">NT$ {{ $filters.currency(order.total) }}</p>
-            </li>
-          </ul>
+              <p>NT$ {{ $filters.currency(item.product.price) }}</p>
+            </div>
+            <div
+              class="
+                d-flex
+                align-items-center
+                justify-content-between
+                border-top
+                pt-3
+              "
+            >
+              <span class="font-md-md fw-bold">總金額：</span
+              ><span class="font-md-md fw-bold">
+                NT$ {{ $filters.currency(order.total) }}
+              </span>
+            </div>
+          </div>
+          <div class="py-2 text-end d-none d-lg-block"></div>
+          <div class="border p-2 py-4 p-lg-4">
+            <h3 class="fw-bold font-md border-bottom pb-3 mb-3">付款狀態</h3>
+            <ul class="list-group-flush">
+              <li class="d-flex mb-2">
+                <p class="w-25 me-1 me-lg-0 text-success fw-bold">付款方式</p>
+                <p class="w-75 fw-bold">信用卡</p>
+              </li>
+              <li class="d-flex">
+                <p class="w-25 me-1 me-lg-0 text-success fw-bold">付款狀態</p>
+                <p
+                  class="bg-success rounded text-white px-2"
+                  v-if="order.is_paid"
+                >
+                  已付款
+                </p>
+                <p class="bg-danger rounded text-white px-2" v-else>未付款</p>
+              </li>
+            </ul>
+          </div>
+          <div class="py-4 text-end d-none d-lg-block">
+            <button type="submit" class="btn btn-success fw-bold">
+              確認付款
+            </button>
+          </div>
         </div>
-        <div class="mb-2 mb-lg-4">
-          <h3 class="h6 mb-2 mb-md-3 fw-bold">付款狀態</h3>
-          <ul class="list-group-flush bg-light py-4">
-            <li class="d-flex mb-2">
-              <p class="me-2">付款方式/</p>
-              <p>信用卡</p>
-            </li>
-            <li class="d-flex">
-              <p class="me-2">付款狀態/</p>
-              <p
-                class="bg-success rounded text-white px-2"
-                v-if="order.is_paid"
-              >
-                已付款
-              </p>
-              <p class="bg-danger rounded text-white px-2" v-else>未付款</p>
-            </li>
-          </ul>
-        </div>
-        <div class="text-end"><!----></div>
       </div>
     </div>
   </div>
@@ -125,3 +141,10 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.order-img {
+  width: 100px;
+  height: 100px;
+}
+</style>
